581c0604642fe9c6223527eb64e9f67be07b6819
Chinmay <singhchinmay207@gmail.com>
2023-10-22 00:12:03 +0530

    push5

--- wellness_whishperer/app.js
+++ wellness_whishperer/app.js
@@ -4,10 +4,15 @@ const path=require("path")
 const app=express();
 const hbs=require("hbs")
 const port= 3000;
+require("./public/js/conn");
+const Register=require("./public/js/modelclient")
+const Register1=require("./public/js/modeldoc")
 
 
 
 const static_path=path.join(__dirname,"templates/views")
+const partial_path=path.join(__dirname,"templates/partials")
+hbs.registerPartials(partial_path)
 
 const static_path1=path.join(__dirname,"public")
 app.use(express.static(static_path1))
@@ -51,12 +56,107 @@ app.get("/gynaecologists",(req,res)=>{
 app.get("/pediatricians",(req,res)=>{
     res.render("pediatricians.hbs")
 });
+app.get("/dentist",(req,res)=>{
+    res.render("dentist.hbs")
+});
+app.get("/podiatrist",(req,res)=>{
+    res.render("podiatrist.hbs")
+});
+app.get("/endocrin",(req,res)=>{
+    res.render("endocrin.hbs")
+});
+app.get("/otolary",(req,res)=>{
+    res.render("otolary.hbs")
+});
+app.get("/pulmologists",(req,res)=>{
+    res.render("pulmologists.hbs")
+});
+app.get("/gastro",(req,res)=>{
+    res.render("gastro.hbs")
+});
+app.get("/oncologist",(req,res)=>{
+    res.render("oncologist.hbs")
+});
+app.get("/nephro",(req,res)=>{
+    res.render("nephro.hbs")
+});
+
+
+
 app.get("/login",(req,res)=>{
     res.render("login.hbs")
 });
 app.get("/signup",(req,res)=>{
     res.render("signup.hbs")
 });
+app.get("/choose",(req,res)=>{
+    res.render("choose.hbs")
+});
+app.get("/docsignup",(req,res)=>{
+    res.render("docsignup.hbs")
+});
+app.post("/signup",async(req,res)=>{
+    try{
+        const password=req.body.password;
+        const cpassword=req.body.confirmpassword;
+        if(password===cpassword){
+            const registerEmployee=new Register({
+                
+                name :req.body.name,
+                email:req.body.email,
+                password:password,
+                confirmpassword:cpassword
+            })
+            const registered=await registerEmployee.save();
+            res.status(201).render("home.hbs");
+        }
+        else{
+            res.send("Password are not matching")
+        }
+    }catch(error){
+        res.status(404).send(error);
+    }
+});
+app.post("/docsignup",async(req,res)=>{
+    try{
+        const password=req.body.password;
+        const cpassword=req.body.confirmpassword;
+        if(password===cpassword){
+            const registerEmployee1=new Register1({
+                
+                name :req.body.name,
+                email :req.body.email,
+                speciality :req.body.speciality,
+                educationlevel :req.body.education,
+                address :req.body.address,
+                timings :req.body.timings,
+                phonenumber :req.body.phone,
+                
+                password:password,
+                confirmpassword:cpassword
+            })
+            const registered1=await registerEmployee1.save();
+            res.status(201).render("home.hbs");
+        }
+        else{
+            res.send("Password are not matching")
+        }
+    }catch(error){
+        res.status(404).send(error);
+    }
+});
+app.post("/login",async(req,res)=>{
+    try {
+        const email=req.body.email;
+        const password=req.body.password;
+        const User=await Register.findOne({email:email});
+        if(User.password===password){
+            res.status(201).render("home.hbs");
+        }
+    } catch (error) {
+        res.status(404).send("Invalid login details");
+    }
+})
 app.listen(port,()=>{
     console.log(`server is running at port ${port}`)
 })
